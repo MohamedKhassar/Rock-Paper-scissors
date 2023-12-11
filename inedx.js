11c8ea5a698559ea66a998708dc16dc56c1c3b29
khassarMohamed <mohamedkhassar775@gmail.com>
2023-12-11 21:30:48 +0100

    create the logic of the game

--- inedx.js
+++ inedx.js
@@ -1,38 +1,87 @@
 let play_btn = document.querySelector(".play-btn");
-let computer_score=0
-let player_score=0
+let computer_score = 0;
+let player_score = 0;
 play_btn.addEventListener("click", () => {
-  let player = document.querySelector("#player").value
+  let player = document.querySelector("#player").value;
+  document.querySelector(".loading").style.display = "block";
   let random = ["rock", "paper", "scissors"];
-  let game=random[Math.round(Math.random()*random.length)]
-  if(player==="rock" && game==="paper"){
-    computer_score++
-    document.querySelector("#score_computer").innerHTML=computer_score
-}
- else if(player==="paper" && game==="rock"){
-    player_score++
-    document.querySelector("#score_player").innerHTML=player_score
-}
-if(player==="rock" && game==="scissors"){
-    player_score++
- document.querySelector("#score_player").innerHTML=player_score
-  }else if(player==="scissors" && game==="rock"){
-    computer_score++
- document.querySelector("#score_computer").innerHTML=computer_score
-  }
+  let game = random[Math.round(Math.random() * random.length)];
 
- if(player==="paper" && game==="scissors"){
-    computer_score++
-    document.querySelector("#score_computer").innerHTML=computer_score
-  }else if(player==="scissors" && game==="paper"){
-    player_score++
-    document.querySelector("#score_player").innerHTML=player_score
-  }
-  
+  if (player !== "") {
+    setTimeout(() => {
+      document.querySelector(".loading").src = "Animation.gif";
+      // document.querySelector(".loading").src=""
+      document.querySelector(".loading").style.display = "none";
 
+      if (player === "rock" && game === "paper" || player === "scissors" && game === "rock" || player === "paper" && game === "scissors") {
+        computer_score++;
+        document.querySelector("#score_computer").innerHTML = computer_score;
+      } else if (player === "paper" && game === "rock" || player === "rock" && game === "scissors" || player === "scissors" && game === "paper") {
+        player_score++;
+        document.querySelector("#score_player").innerHTML = player_score;
+    } 
+    Toastify({
+      text: `Computer Choose ${game}`,
+      duration: 3000,
+      // destination: "https://github.com/apvarun/toastify-js",
+      newWindow: true,
+      close: true,
+      gravity: "top", // `top` or `bottom`
+      position: "left", // `left`, `center` or `right`
+      stopOnFocus: true, // Prevents dismissing of toast on hover
+      style: {
+        background: "green",
+        text: "capitalize",
+      },
+      onClick: function () {}, // Callback after click
+    }).showToast();
+    Toastify({
+      text: `You Choose ${player}`,
+      duration: 3000,
+      // destination: "https://github.com/apvarun/toastify-js",
+      newWindow: true,
+      close: true,
+      gravity: "top", // `top` or `bottom`
+      position: "right", // `left`, `center` or `right`
+      stopOnFocus: true, // Prevents dismissing of toast on hover
+      style: {
+        background: "green",
+      },
+      onClick: function () {}, // Callback after click
+    }).showToast();
+    if (player === game) {
+      Toastify({
+        text: "Draw",
+        duration: 3000,
+        // destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "center", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "red",
+        },
+        onClick: function () {}, // Callback after click
+      }).showToast();
+    }
+    }, "3000");
+  } else {
+    Toastify({
+      text: "choose something",
+      duration: 3000,
+      // destination: "https://github.com/apvarun/toastify-js",
+      newWindow: true,
+      close: true,
+      gravity: "top", // `top` or `bottom`
+      position: "left", // `left`, `center` or `right`
+      stopOnFocus: true, // Prevents dismissing of toast on hover
+      style: {
+        background: "red",
+      },
+      onClick: function () {}, // Callback after click
+    }).showToast();
+  }
   console.log(player);
   console.log(game);
-
 });
-
-
