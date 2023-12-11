0fdbaa0e33be50d3eaf2b00d0a9bc5f1be8aad54
khassarMohamed <mohamedkhassar775@gmail.com>
2023-12-11 22:11:28 +0100

    resolve the issus

--- inedx.js
+++ inedx.js
@@ -4,73 +4,81 @@ let player_score = 0;
 play_btn.addEventListener("click", () => {
   let player = document.querySelector("#player").value;
   document.querySelector(".loading").style.display = "block";
-  document.querySelector(".result").style.columnGap = "100px"
+  document.querySelector(".result").style.columnGap = "100px";
   let random = ["rock", "paper", "scissors"];
-  let game = [...random][Math.round(Math.random()*random.length)]
-  
+  let game = [...random][Math.round(Math.random() * random.length)];
+
   if (player !== "") {
     setTimeout(() => {
       // document.querySelector(".loading").src = "Animation.gif";
       // document.querySelector(".loading").src=""
       document.querySelector(".loading").style.display = "none";
-      document.querySelector(".computer-result").src = `${game}.png`
-      document.querySelector(".computer-result").style.display = "block"
-      document.querySelector(".player-result").src = `${player}.png`
-      document.querySelector(".player-result").style.display = "block"
-      document.querySelector(".result").style.columnGap = "500px"
+      document.querySelector(".computer-result").src = `${game}.png`;
+      document.querySelector(".computer-result").style.display = "block";
+      document.querySelector(".player-result").src = `${player}.png`;
+      document.querySelector(".player-result").style.display = "block";
+      document.querySelector(".result").style.columnGap = "500px";
 
-      if (player === "rock" && game === "paper" || player === "scissors" && game === "rock" || player === "paper" && game === "scissors") {
+      if (
+        (player === "rock" && game === "paper") ||
+        (player === "scissors" && game === "rock") ||
+        (player === "paper" && game === "scissors")
+      ) {
         computer_score++;
         document.querySelector("#score_computer").innerHTML = computer_score;
-      } else if (player === "paper" && game === "rock" || player === "rock" && game === "scissors" || player === "scissors" && game === "paper") {
+      } else if (
+        (player === "paper" && game === "rock") ||
+        (player === "rock" && game === "scissors") ||
+        (player === "scissors" && game === "paper")
+      ) {
         player_score++;
         document.querySelector("#score_player").innerHTML = player_score;
-    } 
-    Toastify({
-      text: `Computer Choose ${game}`,
-      duration: 3000,
-      // destination: "https://github.com/apvarun/toastify-js",
-      newWindow: true,
-      close: true,
-      gravity: "top", // `top` or `bottom`
-      position: "left", // `left`, `center` or `right`
-      stopOnFocus: true, // Prevents dismissing of toast on hover
-      style: {
-        background: "green",
-        text: "capitalize",
-      },
-      onClick: function () {}, // Callback after click
-    }).showToast();
-    Toastify({
-      text: `You Choose ${player}`,
-      duration: 3000,
-      // destination: "https://github.com/apvarun/toastify-js",
-      newWindow: true,
-      close: true,
-      gravity: "top", // `top` or `bottom`
-      position: "right", // `left`, `center` or `right`
-      stopOnFocus: true, // Prevents dismissing of toast on hover
-      style: {
-        background: "green",
-      },
-      onClick: function () {}, // Callback after click
-    }).showToast();
-    if (player === game) {
+      }
       Toastify({
-        text: "Draw",
+        text: `Computer Choose ${game}`,
         duration: 3000,
         // destination: "https://github.com/apvarun/toastify-js",
         newWindow: true,
         close: true,
         gravity: "top", // `top` or `bottom`
-        position: "center", // `left`, `center` or `right`
+        position: "left", // `left`, `center` or `right`
         stopOnFocus: true, // Prevents dismissing of toast on hover
         style: {
-          background: "red",
+          background: "green",
+          text: "capitalize",
         },
         onClick: function () {}, // Callback after click
       }).showToast();
-    }
+      Toastify({
+        text: `You Choose ${player}`,
+        duration: 3000,
+        // destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "green",
+        },
+        onClick: function () {}, // Callback after click
+      }).showToast();
+      if (player === game) {
+        Toastify({
+          text: "Draw",
+          duration: 3000,
+          // destination: "https://github.com/apvarun/toastify-js",
+          newWindow: true,
+          close: true,
+          gravity: "top", // `top` or `bottom`
+          position: "center", // `left`, `center` or `right`
+          stopOnFocus: true, // Prevents dismissing of toast on hover
+          style: {
+            background: "red",
+          },
+          onClick: function () {}, // Callback after click
+        }).showToast();
+      }
     }, "3000");
   } else {
     Toastify({
